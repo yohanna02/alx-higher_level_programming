262dc7b2fe097233233b1d04e192d52e25154162
Yohanna Abana <yohanaabana02@gmail.com>
2023-12-20 18:03:10 +0100

    square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This module defines a simple Square class.
+"""
+
+
+class Square:
+    """This class represents a square.
+
+    Attributes:
+        None
+
+    Methods:
+        None
+    """
+    pass
