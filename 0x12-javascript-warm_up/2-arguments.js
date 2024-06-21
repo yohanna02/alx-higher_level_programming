74fd9c7102a13d3195169df07a37e14265619dcb
Yohanna Abana <yohanaabana02@gmail.com>
2024-06-21 22:26:31 +0100

    fix

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-if (process.agrv.length < 3) {
+if (process.argv.length < 3) {
   console.log('No argument');
 } else if (process.argv.length >= 3) {
   console.log('Argument found');
