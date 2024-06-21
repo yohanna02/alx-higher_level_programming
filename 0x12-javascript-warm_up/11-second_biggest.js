1f48f18559f9d4d93d45cffd9c9f0ad9792b5f0e
Yohanna Abana <yohanaabana02@gmail.com>
2024-06-21 23:31:42 +0100

    fix

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,16 +3,16 @@ const numbers = process.argv;
 let biggest = parseInt(numbers[2]);
 let secondBiggest = parseInt(numbers[2]);
 if (numbers.length <= 3) {
-    console.log(0);
+  console.log(0);
 } else {
-    for (let i = 0; i < numbers.length - 2; i++) {
-        const number = parseInt(numbers[i + 2]);
-        if (number > biggest) {
-            secondBiggest = biggest;
-            biggest = number;
-        } else if (number !== biggest && number > secondBiggest) {
-            secondBiggest = number;
-        }
+  for (let i = 0; i < numbers.length - 2; i++) {
+    const number = parseInt(numbers[i + 2]);
+    if (number > biggest) {
+      secondBiggest = biggest;
+      biggest = number;
+    } else if (number !== biggest && number > secondBiggest) {
+      secondBiggest = number;
     }
+  }
+  console.log(secondBiggest);
 }
-console.log(secondBiggest);
