6b04e59f56bbd3b7b798d6b6fe3b2596952ce1b0
Yohanna Abana <yohanaabana02@gmail.com>
2024-06-21 23:29:35 +0100

    second largest

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const numbers = process.argv;
+let biggest = parseInt(numbers[2]);
+let secondBiggest = parseInt(numbers[2]);
+if (numbers.length <= 3) {
+    console.log(0);
+} else {
+    for (let i = 0; i < numbers.length - 2; i++) {
+        const number = parseInt(numbers[i + 2]);
+        if (number > biggest) {
+            secondBiggest = biggest;
+            biggest = number;
+        } else if (number !== biggest && number > secondBiggest) {
+            secondBiggest = number;
+        }
+    }
+}
+console.log(secondBiggest);
