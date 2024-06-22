1576e9c9915cfe6e9656295b50d41121ead78821
Yohanna Abana <yohanaabana02@gmail.com>
2024-06-22 01:17:41 +0100

    fixing

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
 const _Square = require('./5-square');
 class Square extends _Square {
-  constructor (size) {
-    super(size);
-  }
-
   charPrint (c) {
     if (c) {
       for (let i = 0; i < this.height; i++) {
