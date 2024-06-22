4192f6010df3808d04ac5d0f9a69c98d54ee921e
Yohanna Abana <yohanaabana02@gmail.com>
2024-06-22 01:27:06 +0100

    fix

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-function reverse (list) {
+function esrever (list) {
   const newList = [];
   for (let i = list.length - 1; i >= 0; i--) {
     newList.push(list[i]);
   }
   return newList;
 }
-module.exports = { reverse };
+module.exports = { esrever };
