a7f02c507e610b8ea63f1201852ff47e09082c80
Yohanna Abana <yohanaabana02@gmail.com>
2024-06-22 02:19:06 +0100

    soring

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const { dict } = require("./101-data.js");
+const newDict = {};
+for (const key in dict) {
+  newDict[dict[key]] = [].concat(newDict[dict[key]] || [], [key]);
+}
+console.log(dict);
+console.log(newDict);
